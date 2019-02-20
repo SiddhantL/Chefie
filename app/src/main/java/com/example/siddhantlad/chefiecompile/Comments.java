5f4f737cdead065dc45a4d4ca5fee6f2cf681a94
Siddhant Lad <siddhantnlad@gmail.com>
2019-02-20 20:54:02 +0530

    19/2/19

---
+++ app/src/main/java/com/example/siddhantlad/chefiecompile/Comments.java
@@ -0,0 +1,21 @@
+/*
+19/2/19
+EditText in Listview added, for action(Can be added for quantity too). The value has to be added to the database(Use comments as reference to add it to the database(AddRecipeFab))
+Then add Filter to listview to search ingredients faster.
+
+Finally Add Database
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
